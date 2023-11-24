9b5178b08cffa3ce5b201185e6d8f3963a36e763
Murilo Silva <112777858+Zafkiel45@users.noreply.github.com>
2023-11-24 14:05:09 -0300

    Update task.jsx

--- todo-list/src/app/components/task.jsx
+++ todo-list/src/app/components/task.jsx
@@ -20,10 +20,12 @@ export const Tasks = () => {
             <ol className="flex flex-col gap-3 items-center py-4 list-none w-4/5 h-full">
                 {task.map((item, index) => {
                     return (
-                        <li  onClick={() => {InterativeElements(index)}} className="w-full desktop:w-[90%] hover:cursor-pointer desktop:h-14 mobileMini:py-3 mobileMini:h-11 items-center flex justify-between text-sm h-10 bg-zinc-700 shadow-md rounded-lg py-2 px-3 text-white" key={index}> 
+                        <li  onClick={() => {InterativeElements(index)}} className="w-full desktop:w-[90%] hover:cursor-pointer desktop:h-14 mobileMini:py-3 mobileMini:h-11 items-center flex justify-between text-sm h-10 bg-zinc-700 shadow-md rounded-lg py-2 px-3 text-white" key={Match.random()}> 
                             <div>{item.nome}</div>
                             <div>
-                                <svg xmlns="http://www.w3.org/2000/svg" className={`${task[index].prioridade} animate-bounce`} height="22" viewBox="0 -960 960 960" width="22"><path d="M480-120q-33 0-56.5-23.5T400-200q0-33 23.5-56.5T480-280q33 0 56.5 23.5T560-200q0 33-23.5 56.5T480-120Zm-80-240v-480h160v480H400Z"/></svg>
+                                <svg xmlns="http://www.w3.org/2000/svg" className={`${task[index].prioridade} animate-bounce`} height="22" viewBox="0 -960 960 960" width="22">
+                                    <path d="M480-120q-33 0-56.5-23.5T400-200q0-33 23.5-56.5T480-280q33 0 56.5 23.5T560-200q0 33-23.5 56.5T480-120Zm-80-240v-480h160v480H400Z"/>
+                                </svg>
                             </div>
                         </li>
                     )
