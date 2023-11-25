9b5f4090cef757e8a477fe17110f0502d8955b1d
Murilo Silva <zafkielc@gmail.com>
2023-11-25 09:53:28 -0300

    Change input layout

--- todo-list/src/app/page.jsx
+++ todo-list/src/app/page.jsx
@@ -9,21 +9,25 @@ import { Modal } from "./components/modal";
 export default function Home() {
 
   const key = 'tasks';
-  const [task, setTask] = useState([])
-  const [name, setName] = useState('')
+  const [task, setTask] = useState([]);
+  const [name, setName] = useState('');
   const [idx, setIdx] = useState(null);
-  const [descrebe, setDescrebe] = useState('')
+  const [descrebe, setDescrebe] = useState('');
   const [sideBar, setSideBar] = useState({
     position:'right-[-200%]',
     display: 'hidden',
-  })
+  });
+  const [toggleSideBar, setToggleSideBar] = useState('left-0');
+  const toggleSideBarFunction = () => {
+    setToggleSideBar('left-[-100%]')
+  }
 
   return (
-    <todoContext.Provider value={{key, task, setTask, setName, name, setSideBar, sideBar, descrebe, setDescrebe, idx, setIdx}}>
-      <div className="flex flex-col gap-4 desktop:w-4/5 w-[90%] overflow-hidden h-[95vh] py-5 rounded-lg shadow-md bg-zinc-800">
+    <todoContext.Provider value={{toggleSideBarFunction , key, task, setTask, setName, name, setSideBar, sideBar, descrebe, setDescrebe, idx, setIdx}}>
+      <div className="flex gap-4 w-screen overflow-hidden h-screen py-5 rounded-lg shadow-md bg-zinc-900">
         <Modal/>
-        <div className="flex text-white justify-center w-full font-bold items-center pt-2 flex-col">
-          <span className="mobileMini:text-lg desktopMini:text-2xl">Lista de Tarefas</span>
+        <div className={`flex text-white justify-center absolute top-0 transition-all ${toggleSideBar} h-screen bg-zinc-800 w-screen z-10 font-bold items-center pt-2 flex-col`}>
+
           <InputTask/>   
         </div>
         <hr className="border-zinc-600" />
