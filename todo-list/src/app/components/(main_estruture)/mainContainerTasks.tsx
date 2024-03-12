e8649f3ed9997e79b254c59f8784374d1cd05e1f
zafkiel45 <zafkielc@gmail.com>
2024-03-12 16:31:02 -0300

    refatore: refatored page file. Status ok!

---
+++ todo-list/src/app/components/(main_estruture)/mainContainerTasks.tsx
@@ -0,0 +1,11 @@
+import { Tasks } from "../task"
+
+export const MainContainerTasks = ({blur}:{blur:boolean}) => {
+    return (
+        <div className={`transition-all flex tablet:justify-end tablet:w-[103vw] w-[90vw] ${blur ? 'blur-sm':null} overflow-hidden h-screen py-2 bg-white dark:bg-zinc-900`}>
+            <div className="h-full overflow-y-scroll overflow-x-hidden desktopBig:w-[70%] w-full tablet:w-[60%] flex justify-center">
+                <Tasks/>       
+            </div>
+        </div>
+    )
+}
--- todo-list/src/app/page.tsx
+++ todo-list/src/app/page.tsx
@@ -1,10 +1,11 @@
 'use client'
 import { todoContext } from "./components/context"
-import { InputTask } from "./components/input"
 import { useState } from "react";
-import { Tasks } from "./components/task";
 import { Modal } from "./components/modal";
 import { AllElements } from "./components/utility/fatherMode";
+import { CloseTasksButton } from "./components/(main_estruture)/closeTasksButton";
+import { MainContainerInputs } from "./components/(main_estruture)/mainContainerInputs";
+import { MainContainerTasks } from "./components/(main_estruture)/mainContainerTasks";
 
 export default function Home() {
 
@@ -44,20 +45,10 @@ export default function Home() {
         setIdx
       }}>
         <div className="flex ">
-          <div onClick={toggleSideBarFunctionReverse} className={`px-2 border-r justify-center border-r-gray-300 dark:border-r-zinc-800 w-[10vw] tablet:hidden bg-gray-200 dark:bg-zinc-900 h-screen flex items-center`}>
-            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi fill-zinc-500 bi-arrow-right" viewBox="0 0 16 16">
-              <path fillRule="evenodd" d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8"/>
-            </svg>
-          </div>
-          <div className={`flex text-black dark:text-white tablet:border-r border-r-gray-300 dark:border-r-zinc-800 justify-center absolute top-0 tablet:left-0 transition-all ${toggleSideBar} h-screen bg-white dark:bg-zinc-900 desktopBig:w-[30vw]  tablet:w-[40vw] w-screen z-10 font-bold items-center pt-2 flex-col`}>
-            <InputTask/>   
-          </div>
+          <CloseTasksButton toggleSideBarFunctionReverse={toggleSideBarFunctionReverse} />
+          <MainContainerInputs toggleSideBar={toggleSideBar} />
           <Modal/>
-          <div className={`transition-all flex tablet:justify-end tablet:w-[103vw] w-[90vw] ${blur ? 'blur-sm':null} overflow-hidden h-screen py-2 bg-white dark:bg-zinc-900`}>
-            <div className="h-full overflow-y-scroll overflow-x-hidden desktopBig:w-[70%] w-full tablet:w-[60%] flex justify-center">
-              <Tasks/>       
-            </div>
-          </div>
+          <MainContainerTasks blur={blur} />
         </div>
       </todoContext.Provider>
     </AllElements>
